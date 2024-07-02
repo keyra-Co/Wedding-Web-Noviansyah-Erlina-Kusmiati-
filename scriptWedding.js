1ae3fdd05ec56f8e5afc2ccb7f531d47d3cee3d9
BagasSaputra666 <bagasbagass891@gmail.com>
2024-07-02 20:20:54 +0700

    fix bug browser old

--- scriptWedding.js
+++ scriptWedding.js
@@ -13,34 +13,7 @@ simplyCountdown('.simply-countdown', {
     minutes: { singular: 'menit', plural: 'menit' },
     seconds: { singular: 'detik', plural: 'detik' },
   },
-  // plural: true, //use plurals
-  // inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-  // inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
-  // // in case of inline set to false
-  // enableUtc: false,
-  // onEnd: function () {
-  //     // your code
-  //     return;
-  // },
-  // refresh: 1000, //default refresh every 1s
-  // sectionClass: 'simply-section', //section css class
-  // amountClass: 'simply-amount', // amount css class
-  // wordClass: 'simply-word', // word css class
-  // zeroPad: false,
-  // countUp: false, // enable count up if set to true
 });
-
-// Offcanvas navbar
-// const stickyTop = document.querySelector('.sticky-top');
-// const offCanvas = document.querySelector('.offcanvas');
-
-// offCanvas.addEventListener('show.bs.offcanvas', function () {
-//   stickyTop.style.overflow = 'visible';
-// });
-
-// offCanvas.addEventListener('hidden.bs.offcanvas', function () {
-//   stickyTop.style.overflow = 'hidden';
-// });
 
 // Play audio
 const rootElement = document.querySelector(':root');
@@ -49,24 +22,13 @@ const audioIcon = document.querySelector('.audio-icon-wrapper i');
 let isPlaying = 'false';
 const song = document.querySelector('#song');
 const enable = document.querySelector('.enable');
-
-// function disableScroll() {
-//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-
-//   window.onscroll = function () {
-//     window.scrollTo(scrollTop, scrollLeft);
-//   };
-
-//   rootElement.style.scrollBehavior = 'auto';
-// }
+rootElement.style.scrollBehavior = 'smooth';
 
 enable.onclick = function () {
   window.onscroll = function () {};
 
   rootElement.style.scrollBehavior = 'smooth';
 
-  // localStorage.setItem('opened', 'true')
   playAudio();
 };
 
@@ -93,19 +55,3 @@ audioIconWrapper.onclick = function () {
 
   isPlaying = !isPlaying;
 };
-
-// if (!localStorage.getItem('opened')) {
-//   disableScroll();
-// }
-
-// disableScroll();
-
-// Params
-// const urlParams = new URLSearchParams(window.location.search);
-// const nama = urlParams.get('n') || '';
-// const pronoun = urlParams.get('p') || 'Bapak/Ibu/Saudara/i';
-
-// const namaContainer = document.querySelector('.hero h4 span');
-// namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ',');
-
-// document.querySelector('#nama').value = nama;
